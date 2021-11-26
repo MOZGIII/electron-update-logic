9147fe888a36bc3d51eb90556509b593a264c422
MOZGIII <mike-n@narod.ru>
2021-11-26 16:32:08 +0300

    Correct exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 export * from "./AutoUpdater";
+export { default as AutoUpdater } from "./AutoUpdater";
+
 export * from "./MetaRepo";
+export { default as MetaRepo } from "./MetaRepo";
 
 export * as repo from "./meta/repo";
